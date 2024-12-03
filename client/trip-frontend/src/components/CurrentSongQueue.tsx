bdf8b5268f6702790db600a069b7991580091026
siwuli <siwu430@gmail.com>
2024-12-03 03:06:46 -0500

    current song queue ui changes

--- client/trip-frontend/src/components/CurrentSongQueue.tsx
+++ client/trip-frontend/src/components/CurrentSongQueue.tsx
@@ -3,20 +3,24 @@ import React from 'react';
 interface SongQueueProps {
     songs: string[]; // Function that takes an array of strings and returns nothing (void)
 }
+
 // TODO update the song names with actual added songs
-const CurrentSongQueue: React.FC<SongQueueProps>= ( {songs}) => {
+const CurrentSongQueue: React.FC<SongQueueProps> = ({songs}) => {
     // Hardcoded list of song names
 
     return (
-        <div className="bg-gray-100 rounded-md mt-2 w-2/3 max-h-64 flex flex-col">
+        <div className="bg-gray-100 rounded-md mt-2 w-2/5 flex flex-col h-[75vh]">
             <div className="bg-gray-200 p-4 rounded-t-md">
-                <h2 className="text-xl font-semibold">Current Song Queue</h2>
+                <h2 className="text-lg font-semibold mb-2 text-center">Current Song Queue</h2>
             </div>
 
             <div className="flex-1 p-4 overflow-y-auto">
                 <ul className="space-y-1">
                     {songs.map((song, index) => (
-                        <li key={index} className="text-gray-700">
+                        <li
+                            key={index}
+                            className="text-gray-700 py-1 px-2 bg-white rounded shadow-sm hover:bg-gray-50"
+                        >
                             {index + 1}. {song}
                         </li>
                     ))}
