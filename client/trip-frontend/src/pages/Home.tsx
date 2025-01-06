3082b59bf69ea1c780f72fcdf8adf1ab8187e2e8
limingwei <weilim1996@163.com>
2025-01-06 19:13:27 +0800

    minor typo

--- client/trip-frontend/src/pages/Home.tsx
+++ client/trip-frontend/src/pages/Home.tsx
@@ -66,6 +66,7 @@ export const Home = () => {
                         <AudioPlayer songs={currentQueue}/>
                         <CurrentSongQueue songs={currentQueue}/>
                     </div>
+
                     {/* Text Input at the bottom */}
                     <div className="flex justify-center pb-4 px-4">
                         <div className="w-full">
@@ -74,6 +75,7 @@ export const Home = () => {
                     </div>
                 </div>
                 <PlayLists handleAddToQueue={handleAddToQueue} />
+
             </div>
 
             {/* TODO Update Modal for more Error Messages */}
