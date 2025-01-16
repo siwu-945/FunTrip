55b72cbddd550d2b39d2c364d64b5231fd34335b
siwuli <siwu430@gmail.com>
2025-01-16 00:43:10 -0500

    don't display audio player when no songs were added in the queue.

--- client/trip-frontend/src/components/AudioPlayer.tsx
+++ client/trip-frontend/src/components/AudioPlayer.tsx
@@ -2,13 +2,7 @@ import React, {useEffect, useState} from "react";
 
 const AudioPlayer : React.FC<{songs : SpotifyApi.PlaylistTrackObject[]}> = ({songs}) => {
     const[currentSongId, setCurrentSongId] = useState<string>(songs[0]?.track?.id || "");
-    if(songs.length == 0) return;
 
-    const showSongInfo = () => {
-        songs.forEach((song) => {
-            console.log(song.track);
-        })
-    }
     const handleSongEnd = () => {
         const currentIndex = songs.findIndex((song) => song.track?.id === currentSongId);
         const nextSong = songs[currentIndex + 1];
@@ -24,26 +18,32 @@ const AudioPlayer : React.FC<{songs : SpotifyApi.PlaylistTrackObject[]}> = ({son
 
     return (
         <div>
-            <div className="w-full p-4">
-                {
-                    currentSongId && (<iframe
-                        src={`https://open.spotify.com/embed/track/${currentSongId}?utm_source=generator`}
-                        width="100%"
-                        height="100"
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                        loading="lazy"
-                        className="shadow-lg"
-                    ></iframe>)
-                }
-            </div>
-            <div className="p-4">
-                <button
-                    onClick={handleSongEnd}
-                    className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
-                >
-                    Next Song
-                </button>
-            </div>
+            {songs.length === 0 ? (
+                <></>
+            ) : (
+                <>
+                    <div className="w-full p-4">
+                        {currentSongId && (
+                            <iframe
+                                src={`https://open.spotify.com/embed/track/${currentSongId}?utm_source=generator`}
+                                width="100%"
+                                height="100"
+                                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                                loading="lazy"
+                                className="shadow-lg"
+                            ></iframe>
+                        )}
+                    </div>
+                    <div className="p-4">
+                        <button
+                            onClick={handleSongEnd}
+                            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
+                        >
+                            Next Song
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
