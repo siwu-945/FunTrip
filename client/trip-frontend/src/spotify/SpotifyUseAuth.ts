8d0b50cdd279424378a9e9b55fa763ebb1f59f92
siwuli <siwu430@gmail.com>
2025-08-26 23:29:39 -0400

    local storage updates

--- client/trip-frontend/src/spotify/SpotifyUseAuth.ts
+++ client/trip-frontend/src/spotify/SpotifyUseAuth.ts
@@ -1,27 +1,59 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { resolvePath, useSearchParams } from "react-router-dom";
 import { SpotifyAuthCode } from "./SpotifyAuthCode";
 
+interface SpotifyTokens {
+    accessToken: string;
+    refreshToken: string;
+    expiresAt: number;
+}
 export const useAuth = () => {
-    const [searchParams] = useSearchParams();
+    const [searchParams, setSearchParams] = useSearchParams();
     const [authCode, setAuthCode] = useState("");
     const [accessToken, setAccessToken] = useState("");
 
     // set authorization code
     useEffect(() => {
+        const savedTokens = localStorage.getItem('spotify_tokens');
+        if(savedTokens){
+            const tokens : SpotifyTokens = JSON.parse(savedTokens);
+            const now = Date.now();
+
+            if(tokens.expiresAt > now + 5 * 60 * 1000){
+                setAccessToken(tokens.accessToken);
+                return;
+            }
+        }
         const code = searchParams.get("code");
         if (code) {
             setAuthCode(code);
+            setSearchParams(prev => {
+                const newParams = new URLSearchParams(prev);
+                newParams.delete("code");
+                newParams.delete("state");
+                return newParams;
+            });
         }
-    }, [searchParams]);
+    }, []);
 
     // exchange auth code for access token
     useEffect(() => {
+        console.log("auth code changed")
         const fetchAccessToken = async () => {
             try {
-                if (authCode) {
-                    const token = await SpotifyAuthCode(authCode);
-                    setAccessToken(token);
+                if (authCode && !accessToken) {
+                    const response = await SpotifyAuthCode(authCode);
+                    setAccessToken(response.accessToken);
+                    
+                    const tokens: SpotifyTokens = {
+                        accessToken : response.accessToken,
+                        refreshToken : response.refreshToken,
+                        expiresAt : Date.now() + (response.expiresIn * 1000)
+                    }
+
+                    localStorage.setItem('spotify_tokens', JSON.stringify(tokens))
+                    setAccessToken(response.accessToken);
+                    setAuthCode("");
                 }
             } catch (error) {
                 const errorMessage = (error as Error).message || "Error fetching access token.";
@@ -33,6 +65,7 @@ export const useAuth = () => {
             }
         };
         if (authCode) {
+            console.log("auth code is: ", authCode)
             fetchAccessToken();
         }
     }, [authCode]);
